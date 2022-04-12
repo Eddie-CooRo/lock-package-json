06e29940afc8b825a0c09261aee3dd2173a001af
Eddie CooRo <eddiecooro@gmail.com>
2022-04-12 21:02:23 +0430

    chore: Remove useless files

--- app/cli/cli.ts
+++ app/cli/cli.ts
@@ -9,13 +9,3 @@
  * @license: MIT License
  *
  */
-import m from "@app/functions/module";
-import translate from "@translations/translate";
-import logger from "@app/utils/logger";
-
-(async () => {
-	const label = translate("hello", { name: "World" }); // This show "Hello World"! Is a literal template string from en.json
-	const { app } = await m({ text: label });
-
-	logger.info(app());
-})();
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
+	passWithNoTests: true,
 	globals: {
 		"ts-jest": {
 			tsconfig: "tsconfig.json",
